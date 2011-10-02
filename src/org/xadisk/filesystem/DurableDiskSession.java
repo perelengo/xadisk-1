c95d0548ab33ffc47e5ceb18201197eb93a19882
nitin_verma <nitin_verma@943536b7-b96f-eebf-d757-90621b5eb7c2>
2011-10-02 15:52:59 +0000

    Updated the source headers.

--- src/org/xadisk/filesystem/DurableDiskSession.java
+++ src/org/xadisk/filesystem/DurableDiskSession.java
@@ -1,5 +1,5 @@
 /*
-Copyright © 2010,2011 Nitin Verma (project owner for XADisk http://xadisk.java.net/). All rights reserved.
+Copyright © 2010-2011, Nitin Verma (project owner for XADisk http://xadisk.java.net/). All rights reserved.
 
 This source code is being made available to the public under the terms specified in the license
 "Eclipse Public License 1.0" located at http://www.opensource.org/licenses/eclipse-1.0.php.
