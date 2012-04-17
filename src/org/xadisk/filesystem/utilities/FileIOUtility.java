7368d5bf877a5e7df93144a1f3c141bd7d56fbd0
nitin_verma <nitin_verma@943536b7-b96f-eebf-d757-90621b5eb7c2>
2012-04-17 19:01:31 +0000

    Changes for JIRA issue #103.

--- src/org/xadisk/filesystem/utilities/FileIOUtility.java
+++ src/org/xadisk/filesystem/utilities/FileIOUtility.java
@@ -1,5 +1,5 @@
 /*
-Copyright Â© 2010-2011, Nitin Verma (project owner for XADisk https://xadisk.dev.java.net/). All rights reserved.
+Copyright © 2010-2011, Nitin Verma (project owner for XADisk https://xadisk.dev.java.net/). All rights reserved.
 
 This source code is being made available to the public under the terms specified in the license
 "Eclipse Public License 1.0" located at http://www.opensource.org/licenses/eclipse-1.0.php.
@@ -40,9 +40,8 @@ public class FileIOUtility {
                         }
                         srcFC.close();
                         destFC.close();
-                        if (src.delete()) {
-                            break;
-                        }
+						dest.setLastModified(src.lastModified());
+                        deleteFile(src);
                     }
                 }
                 if (interrupted) {
